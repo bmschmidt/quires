bed0ebe2548715dd388ad7a04de61fe290235d4c
Ben Schmidt <bmschmidt@gmail.com>
2022-02-07 10:17:54 -0500

    first webpage push

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,11 @@
-<h1>
-  <a href="demo/code.html">Demo</a>
-</h1>>
+<h3>
+  <a href="demo/code">Demo of code block behavior customized by class</a>
+</h3>
+
+<h3>
+  <a href="demo/observer">Demo of intersection observer watching Para elements.</a>
+</h3>
+
+<h3>
+  <a href="demo/complex">Demo of a complicated markdown test suite passed through this module.</a>
+</h3>
