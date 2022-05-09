2b12ab5b32e6bbd4e956ccd99eeae32dca232154
Ben Schmidt <bmschmidt@gmail.com>
2022-05-08 23:17:28 -0400

    republish

---
+++ docs/_app/pages/scrollership/index.svelte-bd89e70f.js
@@ -0,0 +1,7 @@
+import{S as p,i as d,s as m,e as f,t as l,c as u,a as S,h as o,d as i,g as c,J as _,n}from"../../chunks/index-051b83ce.js";function g(h){let e,a,s;return{c(){e=f("h1"),a=l("Scrollership"),s=l(`
+
+Scrollership is a model for writing stories where interaction is triggered
+by a subject-area DSL.`)},l(t){e=u(t,"H1",{});var r=S(e);a=o(r,"Scrollership"),r.forEach(i),s=o(t,`
+
+Scrollership is a model for writing stories where interaction is triggered
+by a subject-area DSL.`)},m(t,r){c(t,e,r),_(e,a),c(t,s,r)},p:n,i:n,o:n,d(t){t&&i(e),t&&i(s)}}}class b extends p{constructor(e){super(),d(this,e,null,g,m,{})}}export{b as default};
