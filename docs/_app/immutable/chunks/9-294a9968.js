6ee778a920178a2376d883989ab59a01706608e6
Ben Schmidt <bmschmidt@gmail.com>
2023-02-18 17:17:51 -0500

    update docs to used fixed notes

---
+++ docs/_app/immutable/chunks/9-294a9968.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/demo/sidenotes/_page.svelte-11997051.js";export{t as component};
--- docs/_app/immutable/chunks/9-bd6d09dd.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/demo/sidenotes/_page.svelte-4691be64.js";export{t as component};
